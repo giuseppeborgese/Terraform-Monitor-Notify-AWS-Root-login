86bd12043c194f91d2d850bf64111285e0cf5f3f
Giuseppe Borgese <giuseppe.borgese@clavistechnology.com>
2018-05-04 17:42:36 +0200

    add my email

--- var.tf
+++ var.tf
@@ -1,2 +1,2 @@
 variable "region" {  default = "us-east-1" }
-variable "notificationemail" { default = "giuseppe.borgese.ext@siemens.com"}
+variable "notificationemail" { default = "giuseppeborgese@gmail.com"}
